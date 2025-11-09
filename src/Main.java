bcfb7273faa13e0ef569e5a36a6e6870ff9e4f3c
BobmoKlat <yakir123hehr@gmail.com>
2025-11-09 08:59:38 +0200

    added the test class that provided by lecturer. changed the given GUI to drow my random lines

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import biuoop.GUI;
+import biuoop.DrawSurface;
+
+import java.util.Random;
+import java.awt.Color;
+
+
+
+public static void main(String[] args) {
+    SimpleGuiExample example = new SimpleGuiExample();
+    example.drawRandomLines();
+}
---
+++ src/Point.java
@@ -0,0 +1,30 @@
+ public class Point {
+    private double x;
+    private double y;
+    // constructor
+    public Point(double x, double y) {
+        this.x=x;
+        this.y=y;
+    }
+
+     // distance -- return the distance of this point to the other point
+     public double distance(Point other) {
+        double y=0 , x=0 , y2=0 , x2=0;
+        y= this.y;
+        x= this.x;
+        y2 = other.y;
+        x2 = other.x;
+        return Math.sqrt((x-x2)*(x-x2) + (y-y2)*(y-y2));  //a^2 + b^2 = c^2
+     }
+     // equals -- return true is the points are equal, false otherwise
+     public boolean equals(Point other) {
+        if (other != null)
+            return this.x== other.x &&this.y==other.y;
+        return false;
+     }
+
+     // Return the x and y values of this point
+     public double getX() {return this.x;
+     }
+     public double getY() {return this.y; }
+}
